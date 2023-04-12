c515489b135365627c697154791ce6f9e16170fd
Wajiha11 <71877393+Wajiha11@users.noreply.github.com>
2023-04-12 11:59:35 +0100

    Update read_model_tst.m

--- read_model_tst.m
+++ read_model_tst.m
@@ -1,7 +1,7 @@
 clear
 
 %% Put the file name in commas
-h5mfile_in='80372_02CB_04_050_001_01.h5m';
+h5mfile_in='80372_02CB_05_051_001_01.h5m';
 
 %% Read data from accelerometers
 t1=h5read(h5mfile_in,'/1200.31 Hz/Time'); % time axis for accelerometers
@@ -12,12 +12,21 @@ AX4=h5read(h5mfile_in,'/1200.31 Hz/AX.4');
 AX5=h5read(h5mfile_in,'/1200.31 Hz/AX.5');
 AX6=h5read(h5mfile_in,'/1200.31 Hz/AX.6'); % accelerometer at the fixed end
 %% Wave Probes and carriage
-t_wave=h5read(h5mfile_in,'/200.05 Hz/Time'); % time axis for waves
-WAVE_FORE=h5read(h5mfile_in,'/200.05 Hz/WAVE.FORE'); % probe in front of the beam
-WAVE_SB=h5read(h5mfile_in,'/200.05 Hz/WAVE.SB'); % probe parallel to the beam
+t_wave=h5read(h5mfile_in,'/200.05 Hz/Time'); % time axis for the waves
+WAVE_FORE=h5read(h5mfile_in,'/200.05 Hz/WAVE.FORE'); % probe in front of the beam (26.25 m away from wavemaker)
+WAVE_SB=h5read(h5mfile_in,'/200.05 Hz/WAVE.SB'); % probe parallel to the beam (30 m away from wavemaker)
+
+% The time step of the wave elevation 
+dt = t_wave(2000) - t_wave(1999);
+disp(dt);
+
 %% Waveflap wavemaker position
-t3 = h5read(h5mfile_in,'/100.00 Hz/Time');
-flap_pos=h5read(h5mfile_in,'/100.00 Hz/Flap 3 Pos');
+t3 = h5read(h5mfile_in,'/100.00 Hz/Time'); % time axis for the wavemaker
+flap_pos=h5read(h5mfile_in,'/100.00 Hz/Flap 3 Pos'); % position of the waveflap wavemaker
+
+% The time step of the wavemaker motion
+dt3 = t3(2000) - t3(1999);
+disp(dt3);
 
 %% Waveflap Wavemaker position
 plot(t3, flap_pos,'DisplayName', 'Wavemaker position')
